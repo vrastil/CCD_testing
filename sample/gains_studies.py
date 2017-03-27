dbfe91539e3e261406cd1dce3552931f8c5cbac0
Michal Vrastil <vrastil@astro0010.rcf.bnl.gov>
2017-03-27 05:46:20 -0400

    BNL version of dirs

--- sample/gains_studies.py
+++ sample/gains_studies.py
@@ -9,18 +9,20 @@ def load_runs(a_file):
     return tmp
 
 if __name__ == "__main__":
-    DIR = '/home/vrastil/Documents/Brookhaven/scripts/CCD_testing/sample/run_analysis_dirs.txt'
-    OUT = '/home/vrastil/Documents/Brookhaven/test_output/'
- #   RUNS = load_runs(DIR)
-    RUNS = [
-        '/home/vrastil/Documents/Brookhaven/test_data/gains/some_run',
-        '/home/vrastil/Documents/Brookhaven/test_data/gains/4727D'
-        ]
+#    DIR = '/home/vrastil/Documents/Brookhaven/scripts/CCD_testing/sample/run_analysis_dirs.txt'
+    DIR = '/direct/astro+u/vrastil/CCD_testing/CCD_testing/sample/run_analysis_dirs.txt'
+#    OUT = '/home/vrastil/Documents/Brookhaven/test_output/'
+    OUT = '/gpfs/mnt/gpfs01/astro/www/vrastil/TS8_Data_Analysis/Gain_studies/'
+    RUNS = load_runs(DIR)
+#    RUNS = [
+#        '/home/vrastil/Documents/Brookhaven/test_data/gains/some_run',
+#        '/home/vrastil/Documents/Brookhaven/test_data/gains/4727D'
+#        ]
 
     GAININFO = []
     for i, run in enumerate(RUNS):
         GAININFO.append(fh.GainInfo())
         GAININFO[i].add_gain(run)
+	print GAININFO[i]
 
-    print GAININFO
-    plot_gain(GAININFO[1], GAININFO[0], 'Gain', OUT)
+#    plot_gain(GAININFO[1], GAININFO[0], 'Gain', OUT)
