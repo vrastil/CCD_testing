b714aefefa39bea423647504a7bfd8ff4a7e5c68
vrastil <michalvrastil@seznam.cz>
2017-04-03 14:19:50 +0200

    typo in gains

--- sample/gains_studies.py
+++ sample/gains_studies.py
@@ -1,5 +1,5 @@
 import ccdanalyses.file_handling as fh
-from ccdanalyses.plot_handling import plot_gain
+from ccdanalyses.plot_handling import plot_gains
 
 DIR = '/direct/astro+u/vrastil/CCD_testing/CCD_testing/sample/run_analysis_dirs.txt'
 OUT = '/gpfs/mnt/gpfs01/astro/www/vrastil/TS8_Data_Analysis/Gain_studies/'
