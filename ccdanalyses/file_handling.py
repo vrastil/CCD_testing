cb3f11cd8d719ae3eacab2cee2470870f824088f
vrastil <michalvrastil@seznam.cz>
2017-05-23 12:00:52 +0200

    img_info loading

--- ccdanalyses/file_handling.py
+++ ccdanalyses/file_handling.py
@@ -102,15 +102,35 @@ class ImgInfo(object):
 
     def info(self):
         """ return string, info about the image """
-        str_info = self.__repr__() + '\nCCDs order= '
-        fl_info = ''
+        str_info = 'run=%s\nimg_type=%s\ndate=%s\nfiles=\n' % (
+            self.run, self.img_type, self.date)
         for fli in self:
-            str_info += "'%s' " % fli.dev_name
-            fl_info += '\t' + fli.file + '\n'
-
-        return str_info + '\nFiles=\n' + fl_info
+            str_info += "%s=%s\n" % (fli.dev_name, fli.file)
+        return str_info
 
     def sort(self):
         """ sort CCDs: '00', '01',...,'22' """
         self.files.sort(key=lambda x: x.dev_index_tr, reverse=True)
+
+
+def load_info(info_txt):
+    """ Read 'img_info.txt' and return ImgInfo(object).  """
+    with open(info_txt, 'r') as f:
+        content = f.readlines()
+    run = ''; img_type = ''; date = ''; fl_ls = []; ccd_list = []
+
+    for x in content:
+        if x.startswith('run='):
+            run = x.replace('run=', '').rstrip()
+        if x.startswith('img_type='):
+            img_type = x.replace('img_type=', '').rstrip()
+        if x.startswith('date='):
+            date = x.replace('date=', '').rstrip()
+        if x.startswith('S'):
+            a_file = x[4:].rstrip()
+            fl_ls.append(a_file)
+            ccd_list.append((a_file, x[0:3]))
+
+    img = ImgInfo(fl_ls, ccd_list, run=run, img_type=img_type, date=date)
+    return img
 
